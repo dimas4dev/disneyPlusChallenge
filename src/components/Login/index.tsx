426f4a58171d54542282e1f7d0dc3386a5a05698
Dimas Mendoza <damendoza98@gmail.com>
2024-03-05 16:07:19 -0500

    Agregado componente de inicio de sesión

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,15 @@
+import { useInputChange } from "../../hooks/handleInputChange";
+import { Button } from "../core/Button";
+import { InputComponent } from "../core/Input";
 
 export const Login = () => {
+    const { value, onChange } = useInputChange();
+
     return (
-        <>
-        </>
+        <div className="bg-white h-96 w-96 flex justify-center items-center shadow-xl rounded-lg p-6 mx-autoflex flex-col items-center">
+            <InputComponent type="email" placeholder="Ingresa tu Email" onChange={onChange} value={value} id="emailInput" style={{ margin: "12px" }} />
+            <InputComponent type="password" placeholder="Ingresa tu contraseña" onChange={onChange} value={value} id="passwordInput" />
+            <Button text="Iniciar Sesión" style={{ margin: "12px" }} />
+        </div>
     );
 };
