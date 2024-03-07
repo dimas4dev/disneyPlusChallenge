180fbb3241eb20d1b4edd65fca1e93c1ce801c10
Dimas Mendoza <damendoza98@gmail.com>
2024-03-06 20:53:40 -0500

    Añadir funcionalidad de visibilidad de contraseña en el componente Input

--- src/components/core/Input/index.tsx
+++ src/components/core/Input/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     type: string;
@@ -13,13 +14,31 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
     classname?: string;
 }
+export const InputComponent: React.FC<InputProps> = ({ type, classname, ...props }) => {
+    const [inputType, setInputType] = useState(type);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-export const InputComponent: React.FC<InputProps> = (props, { classname }) => {
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+        setInputType(inputType === 'password' ? 'text' : 'password');
+    };
+
     return (
-        <input
-            {...props}
-            className={`border border-gray-300 rounded-lg p-2 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300 hover:bg-gray-50 transition-colors duration-150 ${classname}`}
-        />
+        <div className={`relative ${classname}`}>
+            <input
+                {...props}
+                type={inputType}
+                className="border border-gray-300 rounded-lg p-2 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300 hover:bg-gray-50 transition-colors duration-150 w-full"
+            />
+            {type === 'password' && (
+                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                    {isPasswordVisible ? (
+                        <EyeSlashIcon className="h-5 w-5 text-gray-700 cursor-pointer" onClick={togglePasswordVisibility} />
+                    ) : (
+                        <EyeIcon className="h-5 w-5 text-gray-700 cursor-pointer" onClick={togglePasswordVisibility} />
+                    )}
+                </div>
+            )}
+        </div>
     );
 };
-
